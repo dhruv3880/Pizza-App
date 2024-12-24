0e3260ce7711908294621f5757a5cc1381df95a9
dhruv <dv160603@gmail.com>
2024-12-24 17:25:49 +0530

    Pizza App

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,16 +2,20 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { useCart } from '@/providers/CartProvider'
 import CartListItem from '@/components/CartListItem';
+import Button from '@/components/Button';
 
 const cart = () => {
-    const {items} = useCart();
+    const {items,total} = useCart();
 
 
   return (
-    <View>
+    <View style={{padding:10}}>
+        
         <FlatList data={items} renderItem={({item})=> <CartListItem cartItem={item}/>}
-        contentContainerStyle={{padding:10, gap:10}} 
+        contentContainerStyle={{gap:10}} 
         />
+        <Text style={{marginTop:10,fontSize:15,fontWeight:'500'}}>Total: ${total}</Text>
+        <Button text="Checkout"/>
 
     </View>
   );
